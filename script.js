1c342ae666384a4af1914c27b2782b47e445fefd
Wojciech Szczyrba <benice195@gmail.com>
2017-01-04 16:27:38 +0100

    Karuzela

---
+++ script.js
@@ -0,0 +1,40 @@
+
+    
+$(function() {
+    var carouselList = $("#carousel ul");
+    setInterval(changeSlide, 3000);
+    function changeSlide() {
+        carouselList.animate({'marginLeft': -400}, 900, moveFirstSlide);
+    };
+    function moveFirstSlide(moveFirstSlide) { 
+        
+        var firstItem = carouselList.find("li:first");
+        var lastItem = carouselList.find("li:last");
+        lastItem.after(firstItem);
+        carouselList.css({marginLeft:0});
+    };
+});
+
+
+
+/*
+<<<--- Pierwsze proby --->>>
+
+
+$(function() {
+    var carouselList = $("#carousel ul");
+    
+    setInterval(changeSlide, 3000);
+
+    var changeSlide = carouselList.animate({'marginLeft': -400}, 2000, moveFirstSlide)    
+    
+    
+    function moveFirstSlide(moveFirstSlide) { 
+        
+        var firstItem = carouselList.find("li:first");
+        var lastItem = carouselList.find("li:last");
+        lastItem.after(firstItem);
+        carouselList.css({marginLeft:0});
+    };
+});
+  */  
