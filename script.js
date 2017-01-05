84b1e5b246675809008663559b726c3084149a15
Wojciech Szczyrba <benice195@gmail.com>
2017-01-05 12:24:24 +0100

    Guzik Dalej na karuzeli

--- script.js
+++ script.js
@@ -2,18 +2,26 @@
     
 $(function() {
     var carouselList = $("#carousel ul");
-    setInterval(changeSlide, 3000);
+    setInterval(changeSlide, 6000);
     function changeSlide() {
         carouselList.animate({'marginLeft': -400}, 900, moveFirstSlide);
     };
-    function moveFirstSlide(moveFirstSlide) { 
+    function moveFirstSlide() { 
         
         var firstItem = carouselList.find("li:first");
         var lastItem = carouselList.find("li:last");
         lastItem.after(firstItem);
         carouselList.css({marginLeft:0});
     };
+    $("#button").click(changeSlide());    
+    $(function(){
+        
+        $('button').click(function(){
+            $(moveFirstSlide);
+        });
+    });
 });
+
 
 
 
