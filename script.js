836756513306ce1553e3e5a12b9a9b87496a7d15
Wojciech Szczyrba <benice195@gmail.com>
2017-01-05 21:33:47 +0100

    Dodatek buttonow

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 
     
-$(function() {
+$(function(move) {
     var carouselList = $("#carousel ul");
     setInterval(changeSlide, 6000);
     function changeSlide() {
-        carouselList.animate({'marginLeft': -400}, 900, moveFirstSlide);
+        carouselList.animate({'marginLeft': -400}, 1200, moveFirstSlide);
     };
+    
     function moveFirstSlide() { 
         
         var firstItem = carouselList.find("li:first");
@@ -13,20 +14,51 @@ $(function() {
         lastItem.after(firstItem);
         carouselList.css({marginLeft:0});
     };
-    $("#button").click(changeSlide());    
-    $(function(){
+    $("#buttonForward").click(changeSlide);
+    
+    $("#buttonBackward").click(moveLastSlide);
         
-        $('button').click(function(){
-            $(moveFirstSlide);
-        });
-    });
+        function moveLastSlide() { 
+        
+        var firstItem = carouselList.find("li:first");
+        var lastItem = carouselList.find("li:last");
+        firstItem.before(lastItem);
+        carouselList.css({marginLeft: -400});
+        carouselList.animate({'marginLeft': 0}, 1200);
+    };
 });
 
 
 
 
 /*
+
+function changeBackSlide() {
+        carouselList.animate({'marginLeft': +400}, 1200, moveLastSlide);
+    };
+
+
+
+
 <<<--- Pierwsze proby --->>>
+    function changeSlideBackward() {
+        carouselList.animate({'marginLeft': +400}, 900, moveLastSlide);
+    };
+    function moveLastSlide() { 
+        
+        var firstItem = carouselList.find("li:first");
+        var lastItem = carouselList.find("li:last");
+        firstItem.after(lastItem);
+        carouselList.css({marginLeft:0});
+    };
+    $("#buttonBackward").click(changeSlideBackward());
+    $(function(){
+        $('#buttonBackward').click(function(){
+            $(moveLastSlide);
+        });
+    });
+
+
 
 
 $(function() {
