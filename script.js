921f4a9622c0f2896cca506cd434669d1a349c45
Wojciech Szczyrba <benice195@gmail.com>
2017-01-07 21:19:41 +0100

    Uproszczenia kodu, porzadki

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-
-    
 $(function(move) {
     var carouselList = $("#carousel ul");
     setInterval(changeSlide, 6000);
@@ -8,18 +6,16 @@ $(function(move) {
     };
     
     function moveFirstSlide() { 
-        
         var firstItem = carouselList.find("li:first");
         var lastItem = carouselList.find("li:last");
         lastItem.after(firstItem);
         carouselList.css({marginLeft:0});
     };
+
     $("#buttonForward").click(changeSlide);
-    
     $("#buttonBackward").click(moveLastSlide);
         
         function moveLastSlide() { 
-        
         var firstItem = carouselList.find("li:first");
         var lastItem = carouselList.find("li:last");
         firstItem.before(lastItem);
@@ -28,53 +24,3 @@ $(function(move) {
     };
 });
 
-
-
-
-/*
-
-function changeBackSlide() {
-        carouselList.animate({'marginLeft': +400}, 1200, moveLastSlide);
-    };
-
-
-
-
-<<<--- Pierwsze proby --->>>
-    function changeSlideBackward() {
-        carouselList.animate({'marginLeft': +400}, 900, moveLastSlide);
-    };
-    function moveLastSlide() { 
-        
-        var firstItem = carouselList.find("li:first");
-        var lastItem = carouselList.find("li:last");
-        firstItem.after(lastItem);
-        carouselList.css({marginLeft:0});
-    };
-    $("#buttonBackward").click(changeSlideBackward());
-    $(function(){
-        $('#buttonBackward').click(function(){
-            $(moveLastSlide);
-        });
-    });
-
-
-
-
-$(function() {
-    var carouselList = $("#carousel ul");
-    
-    setInterval(changeSlide, 3000);
-
-    var changeSlide = carouselList.animate({'marginLeft': -400}, 2000, moveFirstSlide)    
-    
-    
-    function moveFirstSlide(moveFirstSlide) { 
-        
-        var firstItem = carouselList.find("li:first");
-        var lastItem = carouselList.find("li:last");
-        lastItem.after(firstItem);
-        carouselList.css({marginLeft:0});
-    };
-});
-  */  
